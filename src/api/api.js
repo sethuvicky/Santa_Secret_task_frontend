d9b03a86c59a0fa832cc29e4268c5ea8a3e00b8b
sethuvicky <75525385+sethuvicky@users.noreply.github.com>
2024-11-24 16:34:11 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/secret-santa",
+  baseURL: "http://localhost:8000/api/secret-santa",
 });
 
 export default api;
